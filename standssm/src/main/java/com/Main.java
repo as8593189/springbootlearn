5f6b9f56dba223137a7fb5712860cf46dc6a4196
Sakura丿snow <980050775@qq.com>
2020-10-24 22:48:19 +0800

    I am stupid

--- standssm/src/main/java/com/Main.java
+++ standssm/src/main/java/com/Main.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @MapperScan("com.Mapper")
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("我人傻了");
 			//springboot启动器
 		SpringApplication.run(Main.class, args);
 	}
